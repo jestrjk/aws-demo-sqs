4b588b1d8756b2761c328f3b0459f77e7996126d
jake.jester <jake.jester@nih.gov>
2019-09-23 21:10:35 -0400

    added cdk to build resources

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,23 @@
+import cdk = require('@aws-cdk/core');
+import sqs = require('@aws-cdk/aws-sqs')
+import ssm = require( '@aws-cdk/aws-ssm')
+
+export class AwsDemoSqsStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    let queue = new sqs.Queue( this, 'aws-demo-sqs')
+
+    new ssm.StringParameter( this, "aws-demo-ssm-queueName", {
+      parameterName: "/bpimb/aws-demo/sqs/name",
+      stringValue: queue.queueName
+    })
+
+    new ssm.StringParameter( this, "aws-demo-ssm-queueArn", {
+      parameterName: "/bpimb/aws-demo/sqs/arn",
+      stringValue: queue.queueArn
+    })
+
+  }
+}
